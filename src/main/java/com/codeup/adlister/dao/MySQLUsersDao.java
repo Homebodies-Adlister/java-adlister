0d3b0e9690bcb476b68e1127b5abd79b851fd1e1
James Spicer <spicer.r.james@gmail.com>
2020-05-06 17:44:16 -0500

    update

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -54,7 +54,7 @@ public class MySQLUsersDao implements Users {
         long newlyCreatedUserId = 0;
         try {
             //inserts sql command into prepared statement
-            PreparedStatement stmt = connection.prepareStatement("INSERT INTO users(username, email, password, zip) VALUES (?, ?, ?, ?);");
+            PreparedStatement stmt = connection.prepareStatement("INSERT INTO users(username, email, password, zip) VALUES (?, ?, ?, ?);", Statement.RETURN_GENERATED_KEYS);
             //sets the username, email, pass and zip for new user
             stmt.setString(1, user.getUsername());
             stmt.setString(2, user.getEmail());

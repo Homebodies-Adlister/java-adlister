1dbe237d4d06e7ad8b3680fda76ffa2678e461a6
Ted Zamarron <ted.zamarron@gmail.com>
2020-05-07 15:31:09 -0500

    working on deleteUser and deleteStore

---
+++ src/main/java/com/codeup/adlister/controllers/DeleteUser.java
@@ -0,0 +1,20 @@
+package com.codeup.adlister.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/user/delete")
+public class DeleteUser extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+    }
+}
